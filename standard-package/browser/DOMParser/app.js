136adfa4ae3e802aeee9328b08cd3fa1500b06b3
mafuyuk <mafuyuk.m@gmail.com>
2018-09-03 12:21:55 +0900

    Fixed display html

--- standard-package/browser/DOMParser/app.js
+++ standard-package/browser/DOMParser/app.js
@@ -1,8 +1,8 @@
 const parser = new DOMParser();
 // <p>本日は晴天</p>
 // <p>明日は大雨</p>
-const stringContainingHTMLSource = "&lt;p&gt;本日は晴天&lt;&frasl;p&gt;\n&lt;p&gt;明日は大雨&lt;&frasl;p&gt;";
+const stringContainingHTMLSource = "&lt;h1&gt;本日は晴天&lt;/h1&gt;\n&lt;h1&gt;明日は大雨&lt;/h1&gt;";
 const doc = parser.parseFromString(stringContainingHTMLSource, "text/html");
 
 const contents = document.getElementById("contents");
-contents.innerHTML = doc.body.innerHTML;
+contents.innerHTML = doc.body.innerText;
