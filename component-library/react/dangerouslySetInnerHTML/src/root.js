d61806902d02ad0b3349713ce00fdb41160f5131
mafuyuk <mafuyuk.m@gmail.com>
2018-09-03 16:27:46 +0900

    Updated

--- component-library/react/dangerouslySetInnerHTML/src/root.js
+++ component-library/react/dangerouslySetInnerHTML/src/root.js
@@ -1,13 +1,26 @@
 import React, {Fragment} from 'react';
 import ReactDOM from "react-dom";
 
+const sourceString = `
+&lt;h1&gt;本日の天気予報&lt;/h1&gt;
+本日は晴天です
+しかし明日には台風が接近することが予想されるので十分に注意してください。
+`;
+
+const convertToHTMLCharacters = (escapedSpecialHTMLCharacters) => {
+    const parser = new DOMParser();
+    const htmlChar = parser.parseFromString(escapedSpecialHTMLCharacters, "text/html").body.innerText;
+    return htmlChar.replace(/\r?\n/g, '<br>');
+};
+
 const Root = (props) => {
+    const htmlChar = convertToHTMLCharacters(props.html);
     return (
         <Fragment>
-            <div className="text" dangerouslySetInnerHTML={{__html: props.html}}></div>
+            <div className="text" dangerouslySetInnerHTML={{__html: htmlChar}}></div>
         </Fragment>
     );
 };
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Root html={"<h1>本日は晴天</h1>"}/>, rootElement);
+ReactDOM.render(<Root html={sourceString}/>, rootElement);
